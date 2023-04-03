dc9bea0bab20c527f90a280799c551ef4a07f946
wedleysilva <wedleysilva2105@gmail.com>
2023-04-03 16:38:17 -0300

    cirei acessorio e cores atrasado e fiz o serializers consumindo API rest

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from . models import Marca,  Categoria
+from . models import Marca, Acessorio, Categoria, Cor
 
 # Register your models here.
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Cor)
+admin.site.register(Acessorio)
--- garagem/models.py
+++ garagem/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(null=True, blank=True, max_length=50)
@@ -16,3 +13,25 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+class Veiculo(models.Model):
+    Marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    Categoria = models.ForeignKey(Categoria,on_delete=models.PROTECT)
+    Cor = models.ForeignKey(Cor, on_delete=models.PROTECT)
+    ano = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
+    def __str__(self):
+        return self.marca.modelo.ano.cor

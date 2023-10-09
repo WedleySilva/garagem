fec7d039377c3c9fe6d20fd05716fbba1442171f
Wedleysilva <Wedleysilva2105@gmail.com>
2023-10-09 16:30:02 -0300

    correção no serializers

--- garagem/serializers/veiculo.py
+++ garagem/serializers/veiculo.py
@@ -6,14 +6,14 @@ from uploader.serializers import ImageSerializer
 from garagem.models import Veiculo
 
 class VeiculoSerializer(ModelSerializer):
-    class Meta:
-        capa_attachment_key = SlugRelatedField(
+    capa_attachment_key = SlugRelatedField(
         source="image",
         queryset=Image.objects.all(),
         slug_field="attachment_key",
         required=False,
         write_only=True,
     )
-        capa = ImageSerializer(required=False, read_only=True)
+    capa = ImageSerializer(required=False, read_only=True)
+    class Meta:
         model = Veiculo
         fields = "__all__"
